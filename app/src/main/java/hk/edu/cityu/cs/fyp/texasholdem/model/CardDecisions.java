650ed880fb69a8c87bc5a9b285aeba37d13fa804
Man Ka Keung <lllllkaing@gmail.com>
2019-01-24 03:03:06 +0800

    CardDecisions

---
+++ app/src/main/java/hk/edu/cityu/cs/fyp/texasholdem/model/CardDecisions.java
@@ -0,0 +1,14 @@
+package hk.edu.cityu.cs.fyp.texasholdem.model;
+
+public class CardDecisions {
+
+    private long cards = 0;
+
+
+    private boolean isFullHouse(){
+        // TODO:
+        return true;
+    }
+
+
+}
