23f69fb639c831bc85bddc6f12619c66b9719eaf
DESKTOP-HROL46J\ManKaKeung <lllllkaing@gmail.com>
2019-01-24 10:39:58 +0800

    CardDecisions

--- app/src/main/java/hk/edu/cityu/cs/fyp/texasholdem/model/CardDecisions.java
+++ app/src/main/java/hk/edu/cityu/cs/fyp/texasholdem/model/CardDecisions.java
@@ -4,8 +4,33 @@ public class CardDecisions {
 
     private long cards = 0;
 
+    private static final int PAIR = 1000;
+    private static final int TWO_PAIRS = 2000;
+    private static final int FULL_HOUSE = 6000;
+    private static final int THREE_OF_A_KIND = 3000;
+    private static final int STRAIGHT = 4000;
+    private static final int FLUSH = 5000;
+    private static final int FOUR_OF_A_KIND = 7000;
+    private static final int STRAIGHT_FLUSH = 8000;
 
-    private boolean isFullHouse(){
+    public void addCard(){
+
+    }
+
+    public void clear(){
+        cards = 0;
+    }
+
+    private boolean isPair() {
+        return true;
+    }
+
+    private boolean isTwo(){
+        return true;
+    }
+
+
+    private boolean isFullHouse() {
         // TODO:
         return true;
     }
